14838ecbce7f49d91c827959425c12fb449e5f54
Ronnie Low <ronnielow@protonmail.com>
2026-03-27 21:29:28 +1100

    feat: setup STB_GLOBAL conflict checks

--- headers/link.h
+++ headers/link.h
@@ -12,5 +12,7 @@ enum st_info_type {
 };
 
 void validate_objmeta(Objdata *head, Args args);
+void stb_global_collision(Hashmap **file_symbols, Hashmap **globals, Objdata *filedata, Args args);
+bool shared_emachine(uint64_t base, uint64_t input);
 
 #endif
