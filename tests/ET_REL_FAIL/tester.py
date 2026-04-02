3eb2bbbfc6620943cbb1228bf02de3bc2474d18c
sw <ronnielow@protonmail.com>
2026-04-03 00:07:23 +1100

    tests: ET_REL tests

---
+++ tests/ET_REL_FAIL/tester.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import subprocess
+import os
+import sys
+
+linker_bin = sys.argv[1]
+
+def test_prom(prom_path: String, bin_path: String, cwd: String):
+    result = subprocess.run(
+        [prom_path, bin_path],
+        capture_output=True, text=True,
+        cwd=cwd
+    )
+    if result.returncode == 0:
+        print(f"failed to identify {bin_path} is not ET_REL")
+        print(result.stdout)
+        print(result.stderr)
+        exit(1)
+
+
+def main():
+    cwd = os.path.dirname(os.path.abspath(__file__))
+
+    result = subprocess.run(
+        ["make", "all", "-C", cwd],
+        capture_output = True, text = True
+    )
+
+    if result.returncode != 0:
+        print("make failed:", result.stderr)
+        sys.exit(1)
+
+    # checking prom against binaries
+
+    for i in range(1,4):
+        test_prom(linker_bin, f"program{i}.out", cwd)
+
+    print("pass")
+
+
+main()
