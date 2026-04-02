5cb7bb8547a0d772e58ef908f566a6a6c1bf7025
suwa <ronnielow@protonmail.com>
2026-04-02 20:03:04 +1100

    tests: add test to check for no stb_conflict (#13)

---
+++ tests/STB_GLOBAL_NOCONFLICT/tester.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import subprocess
+import os
+import sys
+
+linker_bin = sys.argv[1]
+
+def main():
+    cwd = os.path.dirname(os.path.abspath(__file__))
+
+    result = subprocess.run(
+        ["make", "all", "-C", cwd],
+        capture_output = True, text = True
+    )
+    if result.returncode != 0:
+        print("make failed:", result.stderr)
+        sys.exit(1)
+
+    # checking prom against binaries
+    result = subprocess.run(
+        [linker_bin, "one.o", "two.o"],
+        capture_output=True, text=True,
+        cwd=cwd
+    )
+    if result.returncode != 0:
+        print("ERROR returned when there are no STB_GLOBAL conflicts")
+        print(result.stdout)
+        print(result.stderr)
+
+        exit(1)
+    print("pass")
+
+
+main()
