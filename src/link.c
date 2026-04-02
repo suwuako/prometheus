dbb2d7c94d0a66f76d145d94dca27b85aff7f695
suwa <ronnielow@protonmail.com>
2026-04-03 00:10:30 +1100

    verify et_rel (#14)
    * draft: setup draft pr
    
    because i want the branch to exist so it syncs hehe
    
    * feat: add et_rel check
    
    * tests: ET_REL tests
    
    * feat: somehow i havent added *.out files to gitignore
    
    * fix: et_rel test forgot syntax for strings

--- src/link.c
+++ src/link.c
@@ -9,6 +9,10 @@ bool shared_emachine(uint64_t base, uint64_t input) {
     return base == input;
 }
 
+bool etype_is_et_rel(Elf_header *header) {
+    return header->e_type == ET_REL;
+}
+
 // checks for stb_global symbol collision between files.
 // drains the symtab hashmap and inserts into global hashmap
 // collisions between equal values will crash
@@ -42,6 +46,7 @@ void stb_global_collision(Hashmap **file_symbols, Hashmap **globals, Objdata *fi
 // for linking to succeed:
 //
 // TODO:
+// - [ ] all files are relocatable (eheader->e_type is et_rel)
 // - [x] same e_machine
 // - [x] ensure no global bindings are shared between files
 // undefined external references (every symbol needs to have a corresponding address)
@@ -60,6 +65,9 @@ void validate_objmeta(Objdata *head, Args args) {
 
         if (!shared_emachine(shared_machine, eheader.e_machine))
             fatal_error("Different e_machine detected");
+
+        if (!etype_is_et_rel(&eheader))
+            fatal_error("File's e_type is not ET_REL");
 
         // we get a copy of allocated symtab because stb_global calls hashmap_pop 
         // which modifies the hashmap.
